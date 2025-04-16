4220ff4483838c60fa30cd021dffd85cc03c21fd
it1shka <2tishbel@gmail.com>
2025-04-17 01:07:59 +0200

    feat: started rebuilding the configuration using lazy.nvim

--- init.lua
+++ init.lua
@@ -1,89 +1,15 @@
-local function configure_plugins()
-  local Plug = vim.fn['plug#']
-  vim.call('plug#begin', '~/.config/nvim/plugged')
-  Plug 'airblade/vim-gitgutter'
-  Plug 'styled-components/vim-styled-components'
-  Plug 'neoclide/coc.nvim'
-  Plug 'nvim-lua/plenary.nvim'
-  Plug 'scalameta/nvim-metals'
-  Plug 'preservim/nerdtree'
-  Plug 'neoclide/vim-jsx-improve'
-  Plug 'sbdchd/neoformat'
-  Plug 'leafgarland/typescript-vim'
-  Plug 'peitalin/vim-jsx-typescript'
-  Plug 'nvim-telescope/telescope.nvim'
-
-  -- For JSDoc
-  Plug 'heavenshell/vim-jsdoc'
-
-  -- Support fot pascal
-  Plug 'vim-scripts/fpc.vim'
-  vim.g.pascal_fpc = true
-
-  -- Support for SQL
-  Plug 'tpope/vim-dadbod'
-  vim.g.db = 'postgresql:postgres'
-
-  -- Color themes:
-  Plug 'ellisonleao/gruvbox.nvim'
-  Plug 'joshdick/onedark.vim'
-  Plug 'EdenEast/nightfox.nvim'
-  Plug 'diegoulloao/neofusion.nvim'
-  Plug 'scottmckendry/cyberdream.nvim'
-  Plug 'projekt0n/github-nvim-theme'
-  Plug 'Mofiqul/dracula.nvim'
-  Plug 'briones-gabriel/darcula-solid.nvim'
-  Plug 'rktjmp/lush.nvim'
-
-  -- Zig support
-  Plug 'ziglang/zig.vim'
-
-  vim.call('plug#end')
-end
-
-local function configure_metals_for_scala()
-  local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "scala", "sbt", "java" },
-    callback = function()
-      require("metals").initialize_or_attach({})
-    end,
-    group = nvim_metals_group,
-  })
-end
+require("config.lazy")
 
 local function configure_tabs()
   local set = vim.opt
-  set.tabstop = 2
-  set.softtabstop = 2
-  set.shiftwidth = 2
-  set.expandtab = true
-  set.autoindent = true
-end
-
-local function configure_line_numbers()
-  vim.wo.number = true
-  vim.wo.cursorline = true
-  vim.wo.cursorlineopt = 'number'
-  vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white' })
-end
-
-local function configure_suggestions()
-  vim.opt.showmatch = true
-  vim.opt.ignorecase = true
-  vim.opt.hlsearch = true
-  vim.opt.incsearch = true
-  vim.opt.wildmode = 'longest,list'
-end
-
-local function configure_colorscheme()
-  -- vim.cmd "colorscheme dracula"
-  vim.cmd 'colorscheme darcula-solid'
-  vim.cmd 'set termguicolors'
+  vim.opt.tabstop = 2
+  vim.opt.softtabstop = 2
+  vim.opt.shiftwidth = 2
+  vim.opt.expandtab = true
+  vim.opt.autoindent = true
 end
 
 local function configure_key_mappings()
-  -- helper function for key mapping in Lua
   local function map(mode, lhs, rhs, opts)
     local options = { noremap = true, silent = true }
     if opts then
@@ -94,38 +20,9 @@ local function configure_key_mappings()
     end
     vim.keymap.set(mode, lhs, rhs, options)
   end
-  -- actual mappings
-  map('n', '<S-W>', ':NERDTreeToggle<CR>', { desc = 'Toggles NERDTree View' })
+  map('n', '<C-n>', ':NvimTreeToggle<CR>', { desc = 'Toggles nvim-tree panel' })
   map('n', '<C-l>', ':nohlsearch<CR>', { desc = 'Clears built-in text search highlighting' })
 end
 
-local function setup_telescope()
-  -- got from https://github.com/nvim-telescope/telescope.nvim
-  local builtin = require('telescope.builtin')
-  vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-  vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-  vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-end
-
-local function setup_zig()
-  vim.g.zig_fmt_autosave = 0
-end
-
-local function setup_editor()
-  configure_line_numbers()
-  configure_suggestions()
-  configure_plugins()
-  configure_metals_for_scala()
-  configure_colorscheme()
-  configure_key_mappings()
-  setup_telescope()
-  vim.o.scrolloff = 10
-  vim.o.smartindent = true
-  setup_zig()
-  configure_tabs()
-  print('Welcome again, Tikhon')
-end
-
-setup_editor()
-
+configure_tabs()
+configure_key_mappings()
---
+++ lua/plugins/spec-nvim-tree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+      view = {
+        width = 25,
+      },
+      renderer = {
+        group_empty = true,
+      },
+    }
+  end,
+}
